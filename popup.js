ef876e7bf0cf1ddf10e4ef829c1b9375299cc58e
bewf <x3r0sity@gmail.com>
2026-05-15 02:18:09 -0400

    added report an issue button

--- popup.js
+++ popup.js
@@ -1,12 +1,9 @@
 const left = document.getElementById("left");
 const right = document.getElementById("right");
 const enabledButton = document.getElementById("enabledButton");
-
 let mode = "url_only";
 let enabled = true;
-
 function render() {
-
   // enabled button
   if (enabled) {
     enabledButton.className = "button enabled";
@@ -15,7 +12,6 @@ function render() {
     enabledButton.className = "button disabled";
     enabledButton.textContent = "DISABLED";
   }
-
   // mode switch
   if (mode === "all") {
     right.className = "half active";
@@ -25,46 +21,35 @@ function render() {
     right.className = "half inactive";
   }
 }
-
 function save() {
   browser.storage.local.set({
     mode,
     enabled
   });
 }
-
 browser.storage.local.get([
   "mode",
   "enabled"
 ]).then((res) => {
-
   mode = res.mode || "url_only";
-
   enabled =
     res.enabled !== undefined
       ? res.enabled
       : true;
-
   render();
 });
-
 enabledButton.addEventListener("click", () => {
   enabled = !enabled;
-
   save();
   render();
 });
-
 left.addEventListener("click", () => {
   mode = "url_only";
-
   save();
   render();
 });
-
 right.addEventListener("click", () => {
   mode = "all";
-
   save();
   render();
 });
